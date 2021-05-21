650e59ef897d658cfe41f7e06e41431a20b8bb21
jerober <jetskirobertson2@gmail.com>
2021-05-21 13:04:21 -0600

    Lesson 05 BOM Exercises

---
+++ lesson5/js/bom.js
@@ -0,0 +1,26 @@
+// creating variables
+const input = document.querySelector('#favchap');(
+const button = document.querySelector('button');
+const outputList = document.querySelector('#list');
+
+button.addEventListener('click', () => {
+    let li = document.createElement('li');
+    let deleteButton = document.createElement('button');
+    let chapter = input.value;
+
+    li.textContent(chapter);
+    deleteButton.textContent = '❌';
+
+    li.append(deleteButton);
+
+    outputList.appendChild(li);
+
+    deleteButton.addEventListener('click', function() {
+        outputList.removeChild(li);
+        input.focus();
+    });
+
+});
+
+input = '';
+input.focus();
