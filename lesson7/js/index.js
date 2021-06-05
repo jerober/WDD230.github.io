c147b9897c97d3bde4da46b861c9ac166fac78d0
jerober <jetskirobertson2@gmail.com>
2021-06-05 10:57:22 -0600

    Gallery Initial Commit

--- lesson7/js/index.js
+++ lesson7/js/index.js
@@ -9,7 +9,13 @@ window.onresize = () => {if (window.innerWidth > 760) mainNav.classList.remove('
 //Banner on Fridays
 const currentDate = new Date();
 const weekday = new Array(7);
+weekday[0] = "sunday";
+weekday[1] = "monday";
+weekday[2] = "tuesday";
+weekday[3] = "wednesday";
+weekday[4] = "thursday";
 weekday[5] = "friday";
+weekday[6] = "sarurday";
 const day = weekday[now.getDay()];
 if (day == 'friday') {
     document.querySelector(".banner").innerHTML = "Saturday = Preston Pancakes in the Park!  9:00 a.m. @city park pavilion"
