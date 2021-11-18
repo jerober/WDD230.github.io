f207f99b20294cea29c6e58f3dcb9385e3811d71
jerober <jetskirobertson2@gmail.com>
2021-11-17 22:34:31 -0700

    Town page commit

--- lesson3/js/index.js
+++ lesson3/js/index.js
@@ -1,11 +1,13 @@
 // Responsive hamburger menu
-const hamButton = document.querySelector(".ham");
-const mainNav = document.querySelector(".navigation");
-
-hamButton.addEventListener('click', () => {mainNav.classList.toggle('responsive')}, false);
-
-window.onresize = () => {if (window.innerWidth > 760) mainNav.classList.remove('responsive')};
-
+function toggleMenu() {
+    document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
+    
+    }
+    
+    // hamButton.addEventListener('click', () => {mainNav.classList.toggle('responsive')}, false);
+    
+    // window.onresize = () => {if (window.innerWidth > 760) mainNav.classList.remove('responsive')};
+    
 //Banner on Fridays
 const currentDate = new Date();
 const weekday = new Array(7);
