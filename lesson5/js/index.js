70c78f70d7aabdea09d07fc760ba2e03435d083d
jerober <jetskirobertson2@gmail.com>
2021-12-07 19:31:15 -0700

    json

--- lesson5/js/index.js
+++ lesson5/js/index.js
@@ -39,18 +39,18 @@ fetch(requestURL)
             h2.textContent = towns[i].name;
             card.appendChild(h2);
             document.querySelector('div.cards').appendChild(card);
-            let h3 = document.createElement('h3');
-            h3.textContent = towns[i].motto;
-            card.appendChild(h3);
-            let p = document.createElement('p');
-            p.textContent = 'Year Founded: ' + towns[i].yearFounded;
-            card.appendChild(p);
-            let p = document.createElement('p');
-            p.textContent = 'Population: ' + towns[i].currentPopulation;
-            card.appendChild(p);
-            let p = document.createElement('p');
-            p.textContent = 'Average Rainfall: ' + towns[i].averageRainfall;
-            card.appendChild(p);
+            // let h3 = document.createElement('h3');
+            // h3.textContent = towns[i].motto;
+            // card.appendChild(h3);
+            // let p = document.createElement('p');
+            // p.textContent = 'Year Founded: ' + towns[i].yearFounded;
+            // card.appendChild(p);
+            // let p = document.createElement('p');
+            // p.textContent = 'Population: ' + towns[i].currentPopulation;
+            // card.appendChild(p);
+            // let p = document.createElement('p');
+            // p.textContent = 'Average Rainfall: ' + towns[i].averageRainfall;
+            // card.appendChild(p);
         }
     });
 
